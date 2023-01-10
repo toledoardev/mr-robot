c4889d59068b70fa3aeb31167850d2107c021565
toledoardev <alejandrotoledodev@gmail.com>
2023-01-10 15:32:22 -0300

    Header agregado

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../assets/images/mr_robot.png';
+
+const Header = () => {
+  
+  return (
+
+    <header className='centered'>
+      <img src= { logo }  alt= 'logo' />
+    </header>
+  )
+}
+
+export default Header;
